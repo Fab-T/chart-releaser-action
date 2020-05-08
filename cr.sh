14eaff22bff9c75e5b721d25131a60c61ac4fa80
Fab-T <fabien.traquet@gmail.com>
2020-05-08 16:46:10 -0400

    fix a bit

--- cr.sh
+++ cr.sh
@@ -218,11 +218,11 @@ filter_charts() {
 lookup_changed_charts() {
     #look up for changed files in the latest commit
     local changed_files
-	#debug
-	git log -3
-	git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD) -- $repo
-	git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD~1) -- $repo 
-    changed_files=$(git diff-tree --no-commit-id --name-only -r ..origin -- $repo)
+	  #debug
+	  echo $(git log -3)
+	  echo $(git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD) -- $repo)
+	  echo $(git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD~1) -- $repo)
+    changed_files=$(git diff --no-commit-id --name-only -r origin -- $repo)
 
     local fields
     if [[ "$charts_dir" == '.' ]]; then
